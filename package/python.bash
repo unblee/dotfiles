28d24f7e6ef20c3e66a116114f563a752aff3f87
unblee <unblee@users.noreply.github.com>
2018-02-21 17:22:58 +0900

    add rope python package

--- package/python.bash
+++ package/python.bash
@@ -4,9 +4,10 @@
 pip2 install --upgrade --user neovim
 
 # ver 3
-pip3 install --upgrade --user neovim
 pip3 install --upgrade --user jedi
 pip3 install --upgrade --user mypy
-pip3 install --upgrade --user yapf
+pip3 install --upgrade --user neovim
 pip3 install --upgrade --user pygments
+pip3 install --upgrade --user rope
 pip3 install --upgrade --user yamllint
+pip3 install --upgrade --user yapf
