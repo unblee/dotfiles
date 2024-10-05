c5f54a0c945e0929a3ac925c130b4ce8a15fae9c
unblee <unblee@users.noreply.github.com>
2024-10-05 20:58:58 +0900

    Tweak

--- dot/.config/nvim/lua/config/filetype.lua
+++ dot/.config/nvim/lua/config/filetype.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 -- typescript, typescriptreact, javascript, javascriptreact
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
-	command = "setlocal et ts=4 sw=4 sts=4",
+	command = "setlocal et ts=2 sw=2 sts=2",
 })
 
 vim.filetype.add({
