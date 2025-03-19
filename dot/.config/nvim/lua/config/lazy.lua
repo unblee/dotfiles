eb99263392404159710243370475caaeedaca9dc
unblee <unblee@users.noreply.github.com>
2025-03-20 03:58:19 +0900

    Rename

--- dot/.config/nvim/lua/config/lazy.lua
+++ dot/.config/nvim/lua/config/lazy.lua
@@ -3,28 +3,43 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  install = { colorscheme = { "tokyonight" } },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  }
+	spec = {
+		{
+			import = "plugins_notvscode",
+			cond = function()
+				return not vim.g.vscode
+			end,
+		},
+		{
+			import = "plugins_always",
+			cond = true,
+		},
+		{
+			import = "plugins_vscode",
+			cond = function()
+				return vim.g.vscode
+			end,
+		},
+	},
+	install = { colorscheme = { "tokyonight" } },
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 })
